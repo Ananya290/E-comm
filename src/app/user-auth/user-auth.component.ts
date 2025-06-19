0af82b3b51e39bce4d0adc83be255523cb906050
Ananya Shukla <annayashukla.ss290@gmail.com>
2025-06-19 17:56:55 +0530

    uservalidation

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserserviceService } from '../userservices/userservice.service';
-import { SignUp } from '../../datatype';
+import { logIn, SignUp } from '../../datatype';
 
 @Component({
   selector: 'app-user-auth',
@@ -10,21 +10,79 @@ import { SignUp } from '../../datatype';
 })
 export class UserAuthComponent implements OnInit {
   userSignupForm!:FormGroup;
-  
-constructor(private fb: FormBuilder , private userService :UserserviceService){
-  this.userSignupForm = this.fb.group({
-    username :['',Validators.required],
-    email :['',[Validators.required, Validators.email]],
-    password:[' ',Validators.required]
-  })
+  userLoginForm!:FormGroup;
+  includeNumbers: boolean = true;
+  includeCharacters: boolean = true;
+  isuseraccount :boolean =false;
 
-}
- ngOnInit(): void {
-    this.userService.reloaduser();
+  constructor(private fb: FormBuilder , private userService :UserserviceService){
+    
   }
- 
+
+  ngOnInit(): void {
+    this.userSignupForm = this.fb.group({
+      username :['',Validators.required],
+      email :['',[Validators.required, Validators.email]],
+      password:['',Validators.required]
+    })
+    this.userLoginForm = this.fb.group({
+      email :['' , Validators.required, Validators.email],
+      password:['', Validators.required]
+    })
+    this.userService.reloaduser();
+    this.generateAndSetPassword();
+  }
+
+  generateAndSetPassword() {
+    const password = this.generatePassword(7, this.includeNumbers, this.includeCharacters);
+    this.userSignupForm.patchValue({ password });
+  }
+
+  generatePassword(length: number, includeNumbers: boolean, includeCharacters: boolean): string {
+    
+     let chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (includeCharacters) chars += '!@#$%^&*()_+{}:"<>?';
+    if (includeNumbers) chars += '0123456789';
+    let password = '';
+    for (let i = 0; i < length; i++) {
+      password += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return password;
+  }
+
+  onIncludeNumbersChange(checked: boolean) {
+    this.includeNumbers = checked;
+    this.generateAndSetPassword();
+  }
+
+  onIncludeCharactersChange(checked: boolean) {
+    this.includeCharacters = checked;
+    this.generateAndSetPassword();
+  }
+
+  handleIncludeNumbersChange(event: Event) {
+    const checked = (event.target as HTMLInputElement)?.checked;
+    this.onIncludeNumbersChange(!!checked);
+  }
+
+  handleIncludeCharactersChange(event: Event) {
+    const checked = (event.target as HTMLInputElement)?.checked;
+    this.onIncludeCharactersChange(!!checked);
+  }
+
   SignUp(data:SignUp){
     this.userService.userSignUp(data);
-}
+  }
+  userlogin(data :logIn){
+    this.userService.userLogin(data);
+    console.log("userlogin");
+    
+  }
+  hideLogin(){
+    this.isuseraccount=false;
+  }
+  hideSignUp(){
+    this.isuseraccount=true;
+  }
 
 }
--- src/app/userservices/userservice.service.ts
+++ src/app/userservices/userservice.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { SignUp } from '../../datatype';
+import { EventEmitter, Injectable } from '@angular/core';
+import { logIn, SignUp } from '../../datatype';
 import { log } from 'console';
 import { HttpClient } from '@angular/common/http';
 import { Route, Router } from '@angular/router';
@@ -11,6 +11,8 @@ import { BehaviorSubject } from 'rxjs';
 export class UserserviceService {
   userSignupUrl= "http://localhost:3000/user"
   isuserSignUp = new BehaviorSubject<boolean>(false)
+  userDataError = new EventEmitter<boolean>(false)
+  isLoginerror =new BehaviorSubject<boolean>(false)
 
   constructor(private http : HttpClient,private router: Router) { }
   userSignUp(data :SignUp){    
@@ -21,6 +23,25 @@ export class UserserviceService {
          this.router.navigate(['/']); 
       }) 
 }
+userLogin(data : logIn){
+   if (!data.email || !data.password) {
+      this.userDataError.emit(true);
+      console.warn("login failed: email or password missing");
+      return;
+    }
+  return  this.http.get(`http://localhost:3000/user?email=${data.email}&password=${data.password}`,{observe:"response"}).subscribe((result:any)=>{
+ if(result && result.body && result.body.length ){
+        localStorage.setItem('user', JSON.stringify(result.body))
+        this.router.navigate(['/']);  
+        console.log("login success");
+      }
+      else{
+        this.isLoginerror.next(true)
+        console.warn("login failed")
+      }
+    })
+  }
+
 reloaduser(){
   if(localStorage.getItem('user')) {
     console.log("user is logged in----service");
